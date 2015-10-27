dda4bb091fea9269a6a7f6b413e7bc43361297f4
Aaron Feldman <feldmando@gmail.com>
2015-10-27 13:31:07 -0700

    Add test case 0.1
    This is case 0, but H1 and H2 are connected through a router instead of
    being connected directly

---
+++ cases/case0.1.py
@@ -0,0 +1,30 @@
+'''
+Run with 'python caseX.py > caseX.json'
+'''
+
+import json
+
+description = [
+    {'id': 'H1', 'type': 'host'},
+    {'id': 'R1', 'type': 'router'},
+    {'id': 'H2', 'type': 'host'},
+    {
+        'id': 'L1',
+        'type': 'link',
+        'endpoints': ['H1', 'R1'],
+        'rate': 10 * 10**6,
+        'delay': 10 * 10**-3,
+        'buffer': 64 * 10**3 * 8
+        },
+    {
+        'id': 'L1',
+        'type': 'link',
+        'endpoints': ['R1', 'H2'],
+        'rate': 10 * 10**6,
+        'delay': 10 * 10**-3,
+        'buffer': 64 * 10**3 * 8
+        },
+    {'id': 'F1', 'type': 'flow', 'src': 'H1', 'dst': 'H2', 'amount': 20 * 10**6 * 8, 'start': 1},
+    ]
+
+print json.dumps(description)
