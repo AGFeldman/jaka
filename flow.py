7230ee60cafdca8fb471166e07909c5d0e118542
Aaron Feldman <feldmando@gmail.com>
2015-11-04 23:37:58 -0800

    Move sending logic from hosts to flows

--- flow.py
+++ flow.py
@@ -5,23 +5,66 @@ import globals_
 from packet import Packet
 
 
-class RTTE(object):
+class RTTE(float):
     '''
     Keep a Round Trip Time estimate
+    This class should only be used with `from __future__ import division`
     Public members:
         self.estimate
     '''
     def __init__(self):
         # Initial estimate is 100 ms
         self.estimate = 0.1
-        # TODO(agf): Finish implementing
+        self.ndatapoints = 0
+
+    def update_missed_ack(self):
+        if self.ndatapoints == 0:
+            self.estimate += 0.1
+
+    def no_op(self):
+        pass
+
+    def update_rtt_datapoint(self, rtt):
+        self.ndatapoints += 1
+        if self.ndatapoints == 1:
+            self.estimate = rtt
+        else:
+            # See https://www.stat.wisc.edu/~larget/math496/mean-var.html for
+            # this formula for updating means
+            self.estimate = self.estimate + (rtt - self.estimate) / self.ndatapoints
+        # We no longer need to perform updates when acks are missed
+        self.update_missed_ack = self.no_op
+
+    def __add__(self, x):
+        return self.estimate + x
+
+    def __radd__(self, x):
+        return x + self.estimate
+
+    def __sub__(self, x):
+        return self.estimate - x
+
+    def __rsub__(self, x):
+        return x - self.estimate
+
+    def __mul__(self, x):
+        return self.estimate * x
+
+    def __rmul__(self, x):
+        return x * self.estimate
+
+    def __truediv__(self, x):
+        return self.estimate / x
+
+    def __rtruediv__(self, x):
+        return x / self.estimate
 
 
 class Flow(object):
     def __init__(self, id_=None, start=None, amount=None, src_obj=None, dst_obj=None):
         '''
         id_ is e.g. "F1"
-        start is the time that the flow should start, in seconds
+        start is the time that te flow should start, in seconds
         amount is the amount of data to send, in bits
         src_obj is the Host object for the source
         dst_obj is the Host object for the destination
@@ -32,9 +75,8 @@ class Flow(object):
         self.src_obj = src_obj
         self.dst_obj = dst_obj
 
-        self.src_obj.add_flow(self)
-
-        self.packets = []
+        self.packets_to_send = []
+        self.packets_waiting_for_acks = dict()
         self._generate_packets_to_send()
 
         # Used for statistics
@@ -43,12 +85,47 @@ class Flow(object):
         # significantly by making this an array or something
         self.times_packets_were_received = []
 
-    def receive_ack(packet_id):
-        # TODO(agf): Implement
+    def receive_ack(self, packet):
+        assert packet.dst == self.src_obj.id_
+        assert packet.ack
+        globals_.event_manager.log('{} received ack for {}'.format(self.id_, packet))
+        # TODO(agf): Might need to loosen this requirement, since acks could
+        # just be very late, and so a re-send might already have occured
+        assert packet.id_ in self.packets_waiting_for_acks
+        original_packet = self.packets_waiting_for_acks[packet.id_]
+        assert packet.src == original_packet.dst and packet.dst == original_packet.src
+        del self.packets_waiting_for_acks[packet.id_]
 
-    def start_sending():
-        # TODO(agf): Implement
-        pass
+    def send_packet(self, packet):
+        assert not packet.ack
+        self.src_obj.send_packet(packet)
+        self.packets_waiting_for_acks[packet.id_] = packet
+        def ack_is_due():
+            if packet.id_ in self.packets_waiting_for_acks:
+                globals_.event_manager.log(
+                        '{} is due to receive an ack for {}, has not received it'.format(
+                            self.id_, packet.id_))
+                self.packets_to_send.insert(0, packet)
+            else:
+                globals_.event_manager.log(
+                        '{} is due to receive an ack for {}, has already received it'.format(
+                            self.id_, packet.id_))
+        # TODO(agf): Change the wait time
+        globals_.event_manager.add(globals_.TIME_TO_WAIT_UNTIL_ACK, ack_is_due)
+
+    def start_sending(self):
+        def send_packet_and_schedule_next_send():
+            if not self.packets_to_send:
+                return
+            packet = self.packets_to_send.pop(0)
+            self.send_packet(packet)
+            globals_.event_manager.log('{} sent packet {}'.format(self.id_, packet))
+            globals_.event_manager.add(
+                    # TODO(agf): This time between sends should vary. Actually,
+                    # it should depend on window size.
+                    globals_.TIME_BETWEEN_SENDS, send_packet_and_schedule_next_send)
+        globals_.event_manager.add(0, send_packet_and_schedule_next_send)
+        return True
 
     def _generate_packets_to_send(self):
         '''
@@ -58,12 +135,12 @@ class Flow(object):
         if self.amount % globals_.DATA_PACKET_SIZE != 0:
             npackets += 1
         for id_ in xrange(npackets):
-            self.packets.append(Packet(id_=id_,
-                                       src=self.src_obj.id_,
-                                       dst=self.dst_obj.id_,
-                                       size=globals_.DATA_PACKET_SIZE,
-                                       ack=False,
-                                       flow=self))
+            self.packets_to_send.append(Packet(id_=id_,
+                                               src=self.src_obj.id_,
+                                               dst=self.dst_obj.id_,
+                                               size=globals_.DATA_PACKET_SIZE,
+                                               ack=False,
+                                               flow=self))
 
     def schedule_with_event_manager(self):
         def setup():
--- host.py
+++ host.py
@@ -7,26 +7,6 @@ from packet import Packet
 
 
 class Host(Device):
-    def __init__(self, id_):
-        super(Host, self).__init__(id_)
-
-        # A list of packets that we still need to send
-        self.packets_to_send = []
-
-        # Map from packet id to packet object
-        self.packets_waiting_for_acks = dict()
-
-        # The data packets this host sends will have the ids 0, 1, 2, ...
-        self.next_unused_packet_id = 0
-
-        self.in_sending_chain = False
-
-        # A list of flow objects whose source is this host
-        self.flows = []
-
-    def add_flow(self, flow_obj):
-        self.flows.append(flow_obj)
-
     def receive_packet(self, packet):
         # TODO(agf): This line is temporary. Remove it when we support routing tables.
         if packet.dst != self.id_:
@@ -34,56 +14,22 @@ class Host(Device):
 
         assert packet.dst == self.id_
         if packet.ack:
-            # TODO(agf): I'm here
-        # if packet.ack:
-        #     globals_.event_manager.log('{} received ack for {}'.format(self.id_, packet))
-        #     assert packet.id_ in self.packets_waiting_for_acks
-        #     original_packet = self.packets_waiting_for_acks[packet.id_]
-        #     if packet.src == original_packet.dst and packet.dst == original_packet.src:
-        #         del self.packets_waiting_for_acks[packet.id_]
+            packet.flow.receive_ack(packet)
         else:
             # Log reception for statistics
             packet.flow.log_packet_received()
-            # Create an ack packet and insert it to the front of our list of packets to send
-            # So, this ack should be the next packet sent
+            # Send an ack immediately
+            # TODO(agf): Do we really want to send acks immediately?
             ack = Packet(id_=packet.id_,
                          src=self.id_,
                          dst=packet.src,
                          size=globals_.ACK_SIZE,
-                         ack=True)
-            # TODO(agf): Do we really want to send acks immediately?
+                         ack=True,
+                         flow=packet.flow)
             self.send_packet(ack)
 
     def send_packet(self, packet):
         self.get_endpoint_for_outgoing_packet(packet).receive_from_device(packet)
-        # if not packet.ack:
-        #     self.packets_waiting_for_acks[packet.id_] = packet
-        #     def ack_is_due():
-        #         if packet.id_ in self.packets_waiting_for_acks:
-        #             globals_.event_manager.log(
-        #                     '{} is due to receive an ack for {}, has not received it'.format(
-        #                         self.id_, packet.id_))
-        #             self.packets_to_send.insert(0, packet)
-        #         else:
-        #             globals_.event_manager.log(
-        #                     '{} is due to receive an ack for {}, has already received it'.format(
-        #                         self.id_, packet.id_))
-        #     globals_.event_manager.add(globals_.TIME_TO_WAIT_UNTIL_ACK, ack_is_due)
 
     def act(self):
         return False
-        # for flow in
-        # if not self.in_sending_chain and self.packets_to_send:
-        #     self.in_sending_chain = True
-        #     def send_packet_and_schedule_next_send():
-        #         if not self.packets_to_send:
-        #             self.in_sending_chain = False
-        #             return
-        #         packet = self.packets_to_send.pop(0)
-        #         self.send_packet(packet)
-        #         globals_.event_manager.log('{} sent packet {}'.format(self.id_, packet))
-        #         globals_.event_manager.add(
-        #                 globals_.TIME_BETWEEN_SENDS, send_packet_and_schedule_next_send)
-        #     globals_.event_manager.add(0, send_packet_and_schedule_next_send)
-        #     return True
-        # return False
