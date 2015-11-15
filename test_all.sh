03409c406714f18881569de22692cfb011ab2858
Aaron Feldman <feldmando@gmail.com>
2015-11-15 15:31:09 -0800

    Uncomment tests in test_all.sh

--- test_all.sh
+++ test_all.sh
@@ -27,20 +27,20 @@ if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
     status=$last_status
 fi
 
-# # case1 test
-# echo "Running case1"
-# time python run.py cases/case1.json case1_test_output 
-# last_status=$?
-# if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
-#     status=$last_status
-# fi
-# 
-# # case2 test
-# echo "Running case2"
-# time python run.py cases/case2.json case2_test_output
-# last_status=$?
-# if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
-#     status=$last_status
-# fi
+# case1 test
+echo "Running case1"
+time python run.py cases/case1.json case1_test_output 
+last_status=$?
+if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
+    status=$last_status
+fi
+
+# case2 test
+echo "Running case2"
+time python run.py cases/case2.json case2_test_output
+last_status=$?
+if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
+    status=$last_status
+fi
 
 exit $status
