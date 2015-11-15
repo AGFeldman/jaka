f529cf9613897d677267f19f74981bd5ca729c3c
Jalen Green <jpgreen@caltech.edu>
2015-11-14 18:53:39 -0800

    Remove unused globals

--- globals_.py
+++ globals_.py
@@ -5,10 +5,6 @@ ACK_SIZE = 64 * 8  # 64 bytes
 DATA_PACKET_SIZE = 1024 * 8  # 1024 bytes
 LINK_MAX_BITS_IN_SEQUENCE = 10 ** 5
 
-# These are heavy-handed simplifications
-TIME_BETWEEN_SENDS = 1  # 1 second
-TIME_TO_WAIT_UNTIL_ACK = 10  # 10 seconds
-
 DEFAULT_GRAPH_SLIDING_WINDOW_STEP = 0.1  # seconds
 DEFAULT_GRAPH_SLIDING_WINDOW_WIDTH = 1  # seconds
 
