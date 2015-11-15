f7be59cddc87b7ea25389ea764daafda1a861db0
Aaron Feldman <feldmando@gmail.com>
2015-11-15 15:14:50 -0800

    test_all.sh exit status
    If any test in test_all.sh fails, then test_all.sh exits with the status
    fo the first test that failed. Previously, test_all.sh would always exit
    with the status of the last test, regardless if whether other tests
    failed.

--- test_all.sh
+++ test_all.sh
@@ -1,14 +1,41 @@
+status=0
+
 echo "Running flake8"
 flake8 .
+last_status=$?
+if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
+    status=$last_status
+fi
 
 echo "Running generate.sh"
 cd cases
 bash generate.sh > /dev/null
+last_status=$?
+if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
+    status=$last_status
+fi
 
 cd ..
+
 echo "Running case0"
 time python run.py cases/case0.json case0_test_output 
+last_status=$?
+if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
+    status=$last_status
+fi
+
 echo "Running case1"
 time python run.py cases/case1.json case1_test_output 
+last_status=$?
+if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
+    status=$last_status
+fi
+
 echo "Running case2"
 time python run.py cases/case2.json case2_test_output
+last_status=$?
+if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
+    status=$last_status
+fi
+
+exit $status
