1079b71cfad4dadba41dc81b1c5a064d9f0acdab
Aaron Feldman <feldmando@gmail.com>
2015-11-15 15:17:00 -0800

    Name the tests in test_all.sh

--- test_all.sh
+++ test_all.sh
@@ -1,5 +1,6 @@
 status=0
 
+# flake8 test
 echo "Running flake8"
 flake8 .
 last_status=$?
@@ -7,6 +8,7 @@ if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
     status=$last_status
 fi
 
+# generate.sh test
 echo "Running generate.sh"
 cd cases
 bash generate.sh > /dev/null
@@ -17,6 +19,7 @@ fi
 
 cd ..
 
+# case0 test
 echo "Running case0"
 time python run.py cases/case0.json case0_test_output 
 last_status=$?
@@ -24,6 +27,7 @@ if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
     status=$last_status
 fi
 
+# case1 test
 echo "Running case1"
 time python run.py cases/case1.json case1_test_output 
 last_status=$?
@@ -31,6 +35,7 @@ if [ "$status" == "0" ] && [ "$last_status" != "0" ]; then
     status=$last_status
 fi
 
+# case2 test
 echo "Running case2"
 time python run.py cases/case2.json case2_test_output
 last_status=$?
