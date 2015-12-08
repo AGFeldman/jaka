efb9976b42a98c167e063121c66a1410780c98f0
Aaron Feldman <feldmando@gmail.com>
2015-12-08 01:35:02 -0800

    RTT estimate is based on last N observations
    Estimated mean, base RTT, and max RTT are based on the last N
    observations. Before, they were based on all observations ever made.

--- globals_.py
+++ globals_.py
@@ -1,3 +1,6 @@
+from __future__ import division
+
+
 event_manager = None
 stats_manager = None
 
@@ -15,6 +18,11 @@ SWITCH_ROUTING_TABLE_EVERY = 5  # seconds
 INITIAL_ROUTING_TABLE_SWITCH = 0.4  # seconds
 
 INITIAL_RTT_ESTIMATE = 0.1  # seconds
+# Use the last NUM_OBSERVATIONS_FOR_RTTE observations when estimating average RTT
+# We choose this number such that if a flow transmits at 10 Mbps, then we
+# consider round trip time observations made in the last ~1 second
+# TODO(agf): Calibrate
+NUM_OBSERVATIONS_FOR_RTTE = int(1 * 10 * 10 ** 6 / DATA_PACKET_SIZE)
 
 # interval for increasing window size and
 # how long to wait for ack are magic numbers in flow.py
